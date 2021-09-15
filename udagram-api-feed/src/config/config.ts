3d5a67f0b26f9dbe5e8b66c07641287924196d34
Oussama Bouchikhi <45850487+oussamabouchikhi@users.noreply.github.com>
2021-09-15 12:10:24 +0100

    Read creds form env vars

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': '',
-  'password': 'postgres',
-  'database': '',
-  'host': '',
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
   'dialect': 'postgres',
-  'aws_region': 'us-east-1',
-  'aws_profile': 'default',
-  'aws_media_bucket': '',
-  'url': 'http://localhost:8100',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'url': process.env.URL,
   'jwt': {
-    'secret': '',
+    'secret': process.env.JWT_SECRET,
   },
 };
